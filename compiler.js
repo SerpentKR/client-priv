0b0995a22bc240780ca2d9873f7782769693f755
Mason <masond3v@protonmail.com>
2022-12-17 03:58:32 -0500

    Initial commit

---
+++ compiler.js
@@ -0,0 +1,26 @@
+// Credits: Z3DB0Y <3
+
+console.log('Compiling...');
+
+const fs = require('fs');
+const path = require('path');
+const bytenode = require('bytenode');
+
+function recursive(dir = __dirname) {
+    let dirListing = fs.readdirSync(dir, { withFileTypes: true });
+    for (let item of dirListing) {
+        if (item.isDirectory() && path.join(dir, item.name) !== path.join(__dirname, 'node_modules')) {
+            recursive(path.join(dir, item.name));
+        } else if (item.isFile() && item.name.endsWith('.js') && path.join(dir, item.name) !== __filename && item.name != "client.js" /*&& item.name != "preload.js" && item.name != "splashPreload.js"*/) {
+            let filePath = path.join(dir, item.name);
+            console.log(`Compiling ${filePath}`);
+            bytenode.compileFile(filePath, filePath + 'c');
+            fs.unlinkSync(filePath);
+            // Allows preloader to be used in bytenode compile.
+            fs.writeFileSync(filePath, `require('bytenode');\nmodule.exports = require('./${item.name}c');`);
+        } else {
+            console.log(`Skipping ${item.name}`);
+        }
+    }
+}
+recursive();
